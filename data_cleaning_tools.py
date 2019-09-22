27a7df1a0b5a82ed3f50d6fe170da50716e09215
Dan Hahne <contact@danhahne.com>
2019-09-22 14:50:50 -0400

    update/revert data cleaning tools

--- data_cleaning_tools.py
+++ data_cleaning_tools.py
@@ -1,46 +1,30 @@
 import csv
 import io
 
-import textgrid
 
+def yield_increasing_datapoints(data):
+    rows = csv.reader(data, delimiter='\t')
 
-def row_to_tuple(row):
-    return (min(i.minTime
-                for i in row), max(i.maxTime
-                                   for i in row)) + tuple(i.mark for i in row)
+    current_value = 0.0
 
-
-def filter_func(itr):
-    return filter(lambda x: x.mark != "", itr)
+    for row in rows:
+        if float(row[1]) >= current_value:
+            current_value = float(row[1])
+            yield row + [
+                True,
+            ]
+        else:
+            yield row + [
+                False,
+            ]
 
 
 def clean_data(data):
-
-    tg = textgrid.TextGrid()
-    #tg.read(f=input_file)
-
-    print("reading data into 'tempfile'...")
-    with open("tempfile", "wb") as f:
-        f.write(io.BytesIO(data).read())
-    print("done recording, now to tg...")
-    tg.read("tempfile")
-
     output = io.StringIO()
     writer = csv.writer(output)
 
-    names = tuple(
-        filter(lambda x: x not in ["Orthographic", "Interviewer"],
-               tg.getNames()))
-
-    row_iters = list(map(filter_func, (tg.getFirst(n) for n in names)))
-    rows = zip(*row_iters)
-
-    names = ("minTime", "maxTime") + names
-
-    writer.writerow(names)
-
-    for r in filter(lambda x: not all(not i for i in x[2:]),
-                    map(row_to_tuple, rows)):
-        writer.writerow(r)
+    for row in yield_increasing_datapoints(
+            io.StringIO(data.decode()).readlines()):
+        writer.writerow(row)
 
     return output.getvalue().encode()
